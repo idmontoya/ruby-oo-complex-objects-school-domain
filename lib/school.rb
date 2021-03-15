c7460fe5105336dfdf193ee0914a87d20484c064
Isaiah Montoya <idmontoya@yahoo.com>
2021-03-14 23:12:59 -0800

    commit tree 2e7e7ddbb2501fa104e5cc40f349dea4444fb5b4

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def roster
+        @roster
+    end
+    def add_student(students, grade)
+        roster[grade] ||= []
+        roster[grade] << students
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        student_hash = {}
+        @roster.each do |grade, students|
+            student_hash[grade] = students.sort
+        end
+        student_hash
+    end
+
+end
